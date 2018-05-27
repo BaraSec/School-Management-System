920788a40acbfbddea804264354dcea6665a319e
Bara Abraham <barasec@protonmail.com>
2018-05-27 16:27:41 +0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,13 +1,5 @@
 <?php
   require_once('php/header.php');
-
-  // require_once('php/DatabaseAPI.php');
-
-  // $db = new Database();
-
-  //$db->insert("employees", null, array("112314521521421", "Bara Abraham", "Male", "5665466", "2009-02-15", "Headmaster", 564165.2, "head1", sha1("head1pass")));
-  //$db->insert("employees", null, array("14541561", "Rania", "Female", "45666", "2008-03-02", "Teacher", 16514.5, "teacher1", sha1("teacher1pass")));
-  //$db->insert("students", null, array("Ahmad", "Sameer", "Ramo A", "Mairam B", "Technician", "CEO", "454151", "5641564165", 9, "std1@email.com", "pts1@email.com", "std1", sha1("std1pass")));
 ?>
     <main class="bg">
       <section class="full-width styledMain">
